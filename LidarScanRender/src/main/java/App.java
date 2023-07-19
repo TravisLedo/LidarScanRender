adfa50ad8c5871f3da20a5cca4bc3a6460f8c72e
Travis Ledo <travis.ledo@gmail.com>
2023-07-19 19:48:58 -0400

    Update to gradle

---
+++ LidarScanRender/src/main/java/App.java
@@ -0,0 +1,6 @@
+public class App {
+    //JavaFX Gradle projects need to run from a class that does not extend Application in order for the Run button to work.
+    public static void main(String[] args) {
+        MainLogic.main(args);
+    }
+}
